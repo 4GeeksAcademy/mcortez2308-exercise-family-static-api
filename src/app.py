81af438723908b081f30f6253b3757fafde7a190
mcortez2308 <bensirave@gmail.com>
2026-03-30 20:47:56 +0000

    para evaluacion

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
@@ -30,12 +30,40 @@ def sitemap():
 
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
+
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        raise APIException("Invalid JSON body", status_code=400)
+
+    required_keys = ["first_name", "age", "lucky_numbers"]
+    for key in required_keys:
+        if key not in body:
+            raise APIException(f"Missing field: {key}", status_code=400)
+
+    new_member = jackson_family.add_member(body)
+    return jsonify(new_member), 200
+
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member is None:
+        raise APIException("Member not found", status_code=404)
+    return jsonify(member), 200
+
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    deleted = jackson_family.delete_member(member_id)
+    if not deleted:
+        raise APIException("Member not found", status_code=404)
+    return jsonify({"done": True}), 200
 
 
 
